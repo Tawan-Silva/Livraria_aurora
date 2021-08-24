04dc9fadba9ef3eb4f61804678dd18343c399ed6
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2021-08-24 15:35:32 -0300

    Inserindo Dados

---
+++ INSERT_INTO.sql
@@ -0,0 +1,239 @@
+-- Inserindo dados na tabela categoria
+INSERT INTO categoria (nome_categoria) VALUES 
+('Coleções'),
+('Contos'),
+('Ficção Científica.'),
+('Informática'),
+('Manhwa'),
+('Economia'),
+('Literatura Brasileira'),
+('Literatura Estrangeira'),
+('Outros Assuntos');
+
+-- Inserindo dados na tabela cliente
+INSERT INTO cliente (nome,endereco,telefone,email,cpf,login,senha,editora_cnpj) VALUES 
+('Rayssa Laura Elza Ferreira','Travessa General Euclides Figueiredo','79988107685','rayssalauraelzaferreira-91@dicid.com.br','00483353396','rayssa91','W5iuw5EQRb',null),
+('Lucas Kevin Diogo Souza','Rua Antônio de Almeida Marques','35998022024','lucaskevindiogosouza_@click21.com.br','26538354955','lucassouza26','MgCuz6JZXI',null),
+('Manoel Lucas Baptista','Rua Jaciara','65992545002','manoellucasbaptista@autbook.com','56033334302','batista100','DXictBwUqr',null),
+('Márcio Jorge Nogueira','Rua Dezesseis de Maio','84994414271','marciojorgenogueira-78@comanzzaindaia.com.br','41773317679','nogueira78','eLRQ6nlHYP',null),
+('Danilo Jorge Bernardes','Rua Bolívia','84992067106','danilobernardes-73@lojascentro.com.br','48261940640','bernardes73','aozZH4Ezgl',null),
+('Maitê Laura de Paula','Rua Marcolino Pinto Nogueira','63993836333','maitelauradepaula-94@openlink.com.br','417217559','maite94','HAxflOS9to',null),
+('Rayssa Alícia Teresinha Rodrigues','Via de Acesso para Quadras 87- 87A - 87B','62999338114','rayssaarodrigues_@plastic.com.br','85364509591','ciateresinha','P7AllbIoWt',null),
+('Emily Kamilly Bárbara Moura','Jardim Alto São Francisco','67996758776','emilykamillybarbaramoura@texfuse.com.br','92323948202','kamilly200','JtydBHdfAq',null),
+('Luana Mariane Maya Brito','Rua Professor Guedes de Miranda','82981177192','lluanamarianemayabrito@corpoclinica.com.br','82981177192','mayabritoLL','ktXJZGBnEp',null),
+('Heloise Carolina Carvalho','Rua Joaquim Antônio de Carvalho','83998274380','hheloisecarolinacarvalho@grupova.com.br','67565438570','hheloise','Qfy0JSQhMx',null),
+('Julio Oliver dos Santos','Rua Nogueira Braga','21987039097','juliooliverdossantos-72@netpont.com.br','29307720770','juliooverdos1','GNmeQWOib7',null),
+('Márcia Teresinha Carla Assunção','Rua Angá','37989497094','marciaassuncao-70@fortlar.com.br','90331327619','assuncao70','D7gZUpxmab',null),
+('Raul Igor Benício Carvalho','Rua Niterói','65981134715','rauligorbeniciocarvalho@eanac.com.br','75862351205','raulicarvalho','N7FEjRZIap',null),
+('Noah Elias Rodrigues','Rua África do Sul','82993600802','noaheliasrodrigues-76@terra.com.br','92593491922','noahelias','ZAlz7rwEQf',null),
+('Lúcia Analu Stefany Teixeira','Rua Tatajuba','82981274727','luciaanalustefanyteixeira@gmapst.com','05635707416','lucianaluste','KQP0ozjrl8',null),
+('Flávia Emily Allana Caldeira','Rua Estudante José Walter Farias Mindêlo','83991277804','flaviaemilyallanacaldeira-89@signa.net.br','00939869551','flaviacaldeira','dFhYddPIZw',null),
+('Yuri Ruan Pires','Quadra QNL 17 Conjunto F','61985041199','yuriruanpires__yuriruanpires@weatherford.com','20181100045','yuriruanpires_','cN7nCIyEO0',null),
+('Caroline Ayla Bernardes','Rua Teerã','21994365585','ccarolineaylabernardes@truckeixo.com.br','131220846','ccarolineay','XgsWzJfAzK',null),
+('Ian Miguel Vitor da Mata','Praça das Dálias','64981740691','ianmiguelvitordamata@telefonica.com','19210983173','ianmiguel88','MHEaRGWIah',null),
+('Fernando Yuri Filipe Monteiro','Travessa B','79987837592','fernandoyurifilipemonteiro_@owl-ti.com.br','37673359219','monteiroti','DFc5we58',null),
+('Carla Alice Gomes','Rua do João-de-barro','21983481443','carlaalicegomes_@oana.com.br','51162141093','carlagomes55','oFlyewyiNP',null),
+('Diogo Marcos Vinicius Bento Lima','Praça Marechal Floriano Peixoto, s/n','82993614140','diogomarcosviniciusbentolima@odebrecht.com','26884069309','diegolima900','AOhA2eTsdx',null),
+('Nair Manuela Pereira','Rua D 6B','96992501748','nairmanuelapereira-81@iss.com.br','19640380903','nairpereira81','FmwuBzG12A',null),
+('Eduardo Thales Augusto da Rosa','Travessa Almirante Wandenkolk','91983458459','eduardothalesdarosa_@castromobile.com.br','52874371700','eduardorosa','oHKf3l2mrp',null),
+('Gabriela Renata Mirella Souza','Avenida S','63993649336','gabrielarenata79@redacaofinal.com.br','89109379045','gabrisouza','pLlxPgG8cK',null),
+('Samuel Henry Fábio Bernardes','Vila Vitória Régia','85994839397','samuelhenryfabiobernardes-98@liv.com','43711537987','samuelbes50','UaGHjQn2Aj',null),
+('Lucas Heitor Enrico da Mata','Rua José Honório da Silva','83981786226','lucasheitorenricodamata_@jcffactoring.com.br','92553453540','lucasheitor','dP6CuRfTnD',null),
+('Gabriel Mateus Figueiredo','Rua Justino Augusto Folly','22993700091','gabrielmateusfigueiredo_@advogadostb.com.br','33610443677','gabriel0071','ZPdBZhdy0H',null),
+('Maya Tereza Figueiredo','Rua Rosa de Saron','68982788155','mayaterezafigueiredo@usp.br','86154385609','mayausp5','qWL03kMrF2',null),
+('Henrique Gabriel Gomes','Quadra 1206 Sul Alameda 21','63991582939','henriquegabrielgomes_@br.ibm.com','17229838762','henriquegomes06','DjnU1jNNZ6',null),
+('Maitê Silvana Nina Alves','Passagem Santo Antônio','91994365228','mmaitesilvananinaalves@inbox.com','62930793538','maitesalvez10','Ll36gMfyn1',null),
+('Anthony Felipe Pietro Castro','Rua Manoel Serralha','34981341912','anthonyfelipepietrocastro@mega-vale.com','31333216173','anthonycastro02','QhYk2t0DUC',null),
+('Roberto Renato Noah Gomes','Rua 8','63981066222','robertorenatonoahgomes-79@iss.com.br','18282189600','gomesrenato84','laHzPX9ITn',null),
+('Caio Samuel Teixeira','Quadra SQS 415 Bloco F','61995816805','caiosamuelteixeira_@soelegancia.com.br','92111441830','caiosamuel36','5AyCK5doCS',null),
+('Nicole Gabrielly Maitê Novaes','Caminho 10','71986569360','nicolegabriellymaitenovaes@capua.com.br','23486699784','nicogabriel54','74YZuDGqAy',null),
+('Cauê Kaique Mateus Costa','Quadra QNG 6','61998389894','ccauekaiquemateuscosta@alliancarh.com.br','64936606294','cauekaique1454','zn3y9fbAGo',null),
+('Fábio Arthur Filipe da Costa','Rua Euclides Storti','43995204018','fabioarthurfilipedacosta@sanidet.com.br','69223500818','fabiocosta87','huzx8MXBPO',null),
+('Fernanda Bárbara Elza Nascimento','Rua Riachuelo','81993005733','nascimento_1@marquesalcantra.comabdv.com.br','72839054302','fernanda944','8Gj0cg179N',null),
+('Liz Lavínia Peixoto','Rua Manoel Vieira dos Santos','69987494893','llizlaviniapeixoto@solutionimoveis.com.br','71476109974','peixotoliz1','xpgNdMWdHR',null),
+('Leonardo Matheus Enzo Martins','Rua Aldebarã','95998406991','leonardomatheusenzomartins_@pichler.com.br','89817101398','m4rtins54','HxE661G8Cd',null),
+('Yuri Miguel Silva','Porto Dantas','79989219430','hugoenricosamuelpires_@toysbrasil.com.br','10390797170','hugopires545','3PGlKztakN',null),
+('Nair Sueli Baptista','Quadra AC 115','61987381919','nnairsuelibaptista@bitco.cc','83343638692','nnaisuelita14','p1myisSKeP',null);
+
+
+-- Inserindo dados na tabela fornecedor
+INSERT INTO fornecedor (cnpj,razao_social,endereco,telefone,email) VALUES
+('52634178954154','Penkall','Rua Dr. Miguel Penteado','61987654321','penkall105@gmail.com'),
+('65478941523684','Gente','Rua 1 Cor','11956357854','genteeditoraj@hotmail.com'),
+('14235469785412','Companhia das Letras','Rua Esquina','41986574236','companhialetras@gmail.com'),
+('21554654155466','Suma','Rua da Insonia','23981452364','sumaeditora@homail.com'),
+('26354178596325','Rocco','Lago Grimmauld','71974586321','rocco@yahoo.com'),
+('25874635741597','Campus Editora Rj','Rua Pertin','24996358745','edicampus@hotmail.com'),
+('25369785412369','Alta Books','Caminho das pedras','24199975636','altabooks@outlook.com'),
+('85236471582369','Novatec Editora','Ali do Lado','21975365187','novatecedi@gmail.com'),
+('15465121556514','Panini','Cidade Z','25975856455','panani@hotmail.com'),
+('48554411544544','Viz Media','Rua de Tóquio','87958451221','vizmedia@yahoo.com'),
+('35957568452548','Editora Sextante','Quinta Quadra V','22985635742','sextante@outlook.com'),
+('96357412546578','HarperCollins','Rua dos Iludidos','43985257635','harpercollins@gmail.com'),
+('62562655636526','Kodansha Comics','Federação de jura Tempest','65987458484','kodacomics@gmail.com'),
+('35957568452548','Editora Sextante','Quinta Quadra V','22985635742','sextante@outlook.com'),
+('21655621525657','RNewPOP','Rua da Dungeon do Arquiteto','25965974581','rnewpop@gmail.com'),
+('54489874987896','QualityMark','Rua dos Dividendos','45986821545','quality@gmail.com');
+
+-- Inserindo dados na tabela livro_tem_fornecedor 
+INSERT INTO livro_tem_fornecedor(livro_ISBN) VALUES
+('9786589845881'),('9786588843420'),('9788578270698'),('4000000179801'),
+('4000000142171'),('9786555440690'),('9788501304575'),('9786555440973'),
+('9786555440782'),('9788535932683'),('9788532530806'),('9788535224153'),
+('9788550808789'),('9788575227244'),('9788595080805'),('9788595083271'),
+('9786555111101'),('9788542600032'),('9788542601893'),('9786559600304'),
+('9788560280940'),('9788581052434'),('9786555321029'),('9786586799194'),
+('9786586799200'),('9786586799163'),('9788542629880'),('9781974717354'),
+('9781975382353'),('9981975384753'),('9788541403641');
+
+-- Inserindo dados na tabela cliente_livro_tem_fornecedor
+INSERT INTO cliente_tem_livro_tem_fornecedor(livro_tem_fornecedor_livro_ISBN,livro_tem_fornecedor_fornecedor_cod) VALUES
+('9786589845881',1),('9786588843420',2),('9788578270698',3),('4000000179801',4),
+('4000000142171',5),('9786555440690',6),('9788501304575',7),('9786555440973',8),
+('9786555440782',9),('9788535932683',10),('9788532530806',11),('9788535224153',12),
+('9788550808789',13),('9788575227244',14),('9788595080805',15),('9788595083271',16),
+('9786555111101',17),('9788542600032',18),('9788542601893',19),('9786559600304',20),
+('9788560280940',21),('9788581052434',22),('9786555321029',23),('9786586799194',24),
+('9786586799200',25),('9786586799163',26),('9788542629880',27),('9781974717354',28),
+('9781975382353',29),('9981975384753',30),('9788541403641',31);
+
+-- Inserindo dados na tabela editora
+INSERT INTO editora (cnpj,razao_social,endereco,telefone,email) VALUES
+('52634178954154','Penkall','Rua Dr. Miguel Penteado','61987654321','penkall105@gmail.com'),
+('12365478915975','Nova Fronteira','Rua 19 de Maio','87996975844','novafronteira88@yahoo.com.br'),
+('14785236987452','Martins Fontes','Rua Dr. Vila Nova','84985674259','martinsfont4@hotmail.com'),
+('35748521469874','Antofágica','Rua. Gildásio Amado','21987452536','antofa11@oultook.com'),
+('35745625814963','Ciranda Cultural','Rua Torta Nova','68996365412','ciracultural@hotmail.com'),
+('15974135862147','Gente','Rua Clovis Colourido','85995471236','editoragente10@yahoo.com.br'),
+('15935412147859','Galera Record','Rua Debaixo','91975235475','recordgalera@outlook.com'),
+('78423654159745','Gente','Rua Clovis Arco Íris','11985269874','genteeditora@gmail.com'),
+('65478941523684','Gente','Rua 1 Cor','11956357854','genteeditoraj@hotmail.com'),
+('14235469785412','Companhia das Letras','Rua Esquina','41986574236','companhialetras@gmail.com'),
+('26354178596325','Rocco','Lago Grimmauld','71974586321','rocco@yahoo.com'),
+('25874635741597','Campus Editora Rj','Rua Pertin','24996358745','edicampus@hotmail.com'),
+('25369785412369','Alta Books','Caminho das pedras','24199975636','altabooks@outlook.com'),
+('85236471582369','Novatec Editora','Ali do Lado','21975365187','novatecedi@gmail.com'),
+('96357412546578','HarperCollins','Rua dos Iludidos','43985257635','harpercollins@gmail.com'),
+('12579451548545','HarperCollins','Rua dos Iludidos I','43974856325','harpercollinss@gmail.com'),
+('35957568452548','Editora Sextante','Quinta Quadra V','22985635742','sextante@outlook.com'),
+('12395475632581','Panini','Rua dos Hokages','21987564725','panini@oultook.com'),
+('17656556215461','Panini','Vila Oculta da Chuva','24996874523','panini@gmail.com'),
+('15465121556514','Panini','Cidade Z','25975856455','panani@hotmail.com'),
+('21554654155464','Suma','Cidade de Derry','22978536474','sumaedi@yahoo.com.br'),
+('21554654155466','Suma','Rua da Insonia','23981452364','sumaeditora@homail.com'),
+('15451555215654','Rocco','Republica','61987545212','roccojovens@hotmail.com'),
+('21655621525657','RNewPOP','Rua da Dungeon do Arquiteto','25965974581','rnewpop@gmail.com'),
+('16321541554649','RNewPOP','Rua da Korea','81987458564','rnwepop@outlook.com'),
+('48554411544544','Viz Media','Rua de Tóquio','87958451221','vizmedia@yahoo.com'),
+('56415451545645','Viz Media','Rua dos Shinigami','28986354554','mediaviz@yahoo.com.br'),
+('15454156486454','Yen on','Avenida Six','54599215874','yenon@outlook.com'),
+('62562655636526','Kodansha Comics','Federação de jura Tempest','65987458484','kodacomics@gmail.com'),
+('54489874987896','QualityMark','Rua dos Dividendos','45986821545','quality@gmail.com');
+
+-- Inserindo dados na tabela livro
+INSERT INTO livro (nome,autor,data_entrada,ISBN,quantidade,preco) VALUES
+('Box 11 Livros Machados de Assis','Machado de Assis',02/04/2009,'9786589845881',11,100.98),
+('Eu e Deus','Vito Mancuso',01/06/2016,'9786588843420',24,7.99),
+('As crônicas de Nárnia Coleção Completa','Clive Staples Lewis',01/02/2010,'9788578270698',16,67.49),
+('Kit 5 Livros George Orwell','George Orwell',05/09/2018,'4000000179801',36,39.55),
+('Kit 6 Livros Anne de Green Gables','Lucy Maud Montgomery',01/07/2013,'4000000142171',18,58.98),
+('Desobedeça','Maurício Benvenutti',09/12/2017,'9786555440690',26,30.99),
+('Corte de Chamas Prateadas (VOL 4)','Sarah J. Maas',12/11/2020,'9788501304575',17,200.99),
+('Lider Pronto para Tudo','Marcos Scaldelai',14/09/2019,'9786555440973',9,40.00),
+('Gestão Profissional na Prática','Filipe Colombo',30/07/2018,'9786555440782',37,27.98),
+('A Gorota Marcada para Morrer','David Legercrat',30/09/2019,'9788535932683',41,39.89),
+('Harry Potter e o prisioneiro de Azkaban','Joanne Rowling',08/10/2015,'9788532530806',17,52.98),
+('Logistica e Gerenciamento da Cadeia de Distribuição','Antonio Galvão Novaes',03/08/2021,'9788535224153',62,11.20),
+('Dominando o Ciclo de Mercado','Howard Marks',30/03/2020,'9788550808789',12,48.99),
+('Refatoração: Aperfeiçoando o Design de Códigos Existentes','Martin Fowler',30/05/2020,'9788575227244',87,90.89),
+('O investidor inteligente','Benjamin Graham',25/01/2016,'9788595080805',65,39.00),
+('Do Mil ao Milhão. Sem Cortar o Cafezinho','Thiago Nigro',10/11/2018,9788595083271,16,19.90),
+('A Psicologia Financeira','Morgan Housel',15/04/2021,'9786555111101',14,28.90),
+('Naruto (Volume 60)','Masashi Kishimoto',28/04/2013,'9788542600032',19,50.00),
+('Naruto (Volume 70)','Masashi Kishimoto',23/05/2017,'9788542601893',21,54.00),
+('One Punch Man (Volume 23)','Yasuke Murata',02/04/2021,'9786559600304',71,31.00),
+('It: A Coisa','Stephen King',24/06/2014,'9788560280940',25,48.90),
+('Doutor sono','Stephen King',23/09/2014,'9788581052434',47,34.46),
+('Coleção Triologia Legend','Marie Lu',05/09/2014,'9786555321029',14,214.98),
+('Solo Leveling (Volume 01)','Kisoryong Chugong',10/08/2020,'9786586799194',61,42.54),
+('Solo Leveling (Volume 02)','Kisoryong Chugong',24/08/2020,'9786586799200',51,52.90),
+('Solo Leveling (Volume 03)','Kisoryong Chugong',10/04/2021,'9786586799163',41,43.90),
+('Demon Slayer - Kimetsu No Yaiba (Vol 08)','Koyoharu Gotōge',15/09/2020,'9788542629880',56,24.90),
+('The Art of Magic: The Gathering - War of the Spark: 8','James Wyatt',13/09/2020,'9781974717354',9,239.57),
+('Final Fantasy VII: On the Way to a Smile','Kazushige Nojima',30/09/2018,'9781975382353',35,66.65),
+('Attack on Titan (Vol 1)','Hajime Isayama',16/06/2019,'9981975384753',12,45.50),
+('Mercado Financeiro','Eduardo Fortuna',13/11/2020,'9788541403641',02,370.88);
+
+-- Inserindo dados na tabela pedido
+INSERT INTO pedido (nome,quantidade,valor,categoria_ID,fornecedor_ID,ID_cliente) VALUES
+('A Gorota Marcada para Morrer','1','41.99','9','3','1'),
+('A Psicologia Financeira','2','14.79','6','6','6');
+
+INSERT INTO pedido (nome,quantidade,valor,categoria_ID,fornecedor_ID,ID_cliente) VALUES
+('Coleção Triologia Legend','2','215.98','3','5','9'),
+('One Punch Man (Volume 23)','1','31.87','9','9','19'),
+('Doutor sono','2','34.60','9','4','28');
+
+-- Inserindo dados na tabela pedido_nota_fiscal
+INSERT INTO produto_nota_fiscal VALUES ('1', 'Box 11 Livros Machados de Assis', 11),
+('2', 'Eu e Deus', 24),
+('3','As crônicas de Nárnia Coleção Completa', 16),
+('4','Kit 5 Livros George Orwell',36),
+('5','Kit 6 Livros Anne de Green Gables',18),
+('6','Desobedeça',26),
+('7','Corte de Chamas Prateadas (VOL 4)',17),
+('8','Lider Pronto para Tudo',9),
+('9','Gestão Profissional na Prática',37),
+('10','A Gorota Marcada para Morrer',41),
+('11','Harry Potter e o prisioneiro de Azkaban',17),
+('12','Logistica e Gerenciamento da Cadeia de Distribuição',62),
+('13','Dominando o Ciclo de Mercado',12),
+('14','Refatoração: Aperfeiçoando o Design de Códigos Existentes',87),
+('15','O investidor inteligente',65),
+('16','Do Mil ao Milhão. Sem Cortar o Cafezinho',16),
+('18','Naruto (Volume 60)',19),
+('19','Naruto (Volume 70)',21),
+('20','One Punch Man (Volume 23)',71),
+('21','It: A Coisa',25),
+('22','Doutor sono',47),
+('23','Coleção Triologia Legend',14),
+('24','Solo Leveling (Volume 01)',61),
+('25','Solo Leveling (Volume 02)',51),
+('26','Solo Leveling (Volume 03)',41),
+('27','Demon Slayer - Kimetsu No Yaiba (Vol 08)',56),
+('28','The Art of Magic: The Gathering - War of the Spark: 8',9),
+('29','Final Fantasy VII: On the Way to a Smile',35),
+('30','Attack on Titan (Vol 1)',12),
+('31','Mercado Financeiro',2);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
